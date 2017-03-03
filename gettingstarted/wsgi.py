b3f9446a4cd43ab7186cd38e2e855f6fd1b2eefd
Hitch Lab <hoangdov@gmail.com>
2017-03-03 19:01:13 +0000

    add proxy initialization

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import requests
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
@@ -15,3 +17,10 @@ from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+
+proxies = {
+    "http": os.environ['http://quotaguard8995:948b402a5f97@us-east-1-static.quotaguard.com:9293']
+}
+
+res = requests.get("http://ip.jsontest.com/", proxies=proxies)
+print res.text
